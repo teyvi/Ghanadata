b355263bf310d95d9d83a0a0b8b6d9460cfc5085
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-01-23 23:47:52 +0000

    Total amount mobilised 2020

---
+++ TotalAmountMobilised_GhanaCovidFund.py
@@ -0,0 +1,12 @@
+# libraries
+import matplotlib.pyplot as plt
+import squarify    # pip install squarify (algorithm for treemap)
+import pandas as pd
+
+# Create a data frame with fake data
+df = pd.DataFrame({'2020_amount':[1203715085.77,5566676161.54,398745781.93,569617581.78,10000000000.00,827794389.62], 'funding_source':["GOG Contingency Fund", "IMF", "AfDB", "EU", "BOG-COVID-19 BONDS","Other GoG"] })
+
+# plot it
+squarify.plot(sizes=df['2020_amount'], label=df['funding_source'], alpha=.8 )
+plt.axis('off')
+plt.show()
